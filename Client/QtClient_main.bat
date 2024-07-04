f243085fc4cb52e6cfc23fa779b155fecda03372
EC <qcentanglement@gmail.com>
2024-07-04 15:53:49 +0900

    Batch file update
    Update file path in batch file

--- Client/QtClient_main.bat
+++ Client/QtClient_main.bat
@@ -1,5 +1,5 @@
 @echo on
 TITLE QtClientMain_v3.02
 call C:\Users\QC109\Anaconda3\Scripts\activate.bat
-C:\Users\QC109\Anaconda3\python.exe "E:\v3.03\QtClient_main_v0_01.py"
+C:\Users\QC109\Anaconda3\python.exe "C:\Users\QC109\Desktop\GUI_program\remote-entanglement-team\Client\QtClient_main_v0_01.py"
 pause
